c944259a8bf4977aa3d11472ec34b25991911255
KoneruGeetha <80870551+KoneruGeetha@users.noreply.github.com>
2021-08-28 16:28:04 +0530

    Update Climb Stairs.py

--- Climb Stairs.py
+++ Climb Stairs.py
@@ -1,10 +1,10 @@
-Question:
-1. You are given a number n, representing the number of stairs in a staircase.
-2. You are on the 0th step and are required to climb to the top.
-3. In one move, you are allowed to climb 1, 2 or 3 stairs.
-4. You are required to print the number of different paths via which you can climb to the top.
-Code:
-***8***********************************
+# Question:
+# 1. You are given a number n, representing the number of stairs in a staircase.
+# 2. You are on the 0th step and are required to climb to the top.
+# 3. In one move, you are allowed to climb 1, 2 or 3 stairs.
+# 4. You are required to print the number of different paths via which you can climb to the top.
+# Code:
+
 n=int(input())
 dp=[0]*(n+1)
 dp[0]=1
@@ -13,4 +13,4 @@ dp[2]=2
 for i in range(3,n+1):
     dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
 print(dp[n])
-***************************************
+
